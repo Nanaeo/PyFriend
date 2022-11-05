5f4f365abf0517373cd4ad7ba47b5ec46798c708
MliKiowa <nineto0@163.com>
2022-11-05 21:38:54 +0800

    Update __init__.py

--- helper/__init__.py
+++ helper/__init__.py
@@ -3,4 +3,4 @@ from .PluginBase import PluginBase
 from .LogConsole import PrintConsole
 from .GlobalDict import getValue
 from .GlobalDict import setValue
-from .GlobalDict import auth_init
+from .GlobalDict import authInit
