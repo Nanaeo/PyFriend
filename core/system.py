f374d3661d57c47be322f07d59c8add4d7592275
MliKiowa <nineto0@163.com>
2022-11-05 18:05:25 +0800

    Update system.py

--- core/system.py
+++ core/system.py
@@ -1,3 +1,4 @@
+import helper
 def systemLoad():
   helper.printConsole("Welcome","Hello User","The system starts to load and run . ",1)
   helper.printConsole("Check","Vesion",PYFRIEND_INFO_VERSION,1)
