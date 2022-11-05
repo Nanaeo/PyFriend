3cd846cfb1d650d99e6fc0cd6f269a5ab248692f
MliKiowa <nineto0@163.com>
2022-11-05 12:50:05 +0800

    Update Plugin.py

--- plugins/GithubApi/Plugin.py
+++ plugins/GithubApi/Plugin.py
@@ -1,1 +1,8 @@
-
+print("GithubApi Loaded! - Default Message")
+class Plugin:
+  def __init__(self, permissionsCode):
+    self.permissionsCode = permissionsCode
+  def RegisterEvent():
+    return
+  def BindEvent():
+    return
