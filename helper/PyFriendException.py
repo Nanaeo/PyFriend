79c52b52140f4a40ef771c1c6d20ee9ceebaf2db
MliKiowa <nineto0@163.com>
2022-11-05 15:46:11 +0800

    Create PyFriendException.py

---
+++ helper/PyFriendException.py
@@ -0,0 +1,6 @@
+class MyException(Exception):
+    def __init__(self, Type,Location,Msg,Color = 0):
+        self.Type = Type
+        self.Location = Location
+        self.Msg = Msg
+        self.Color = Color
