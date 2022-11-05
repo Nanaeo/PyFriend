35de8bfc5b6047a59869cdeb067834aa68d511a9
MliKiowa <nineto0@163.com>
2022-11-05 10:06:39 +0800

    Update init.py

--- init.py
+++ init.py
@@ -21,15 +21,23 @@ def infoConsole(Type,Location,Msg):
 # Yaml will be fully loaded in Full unsafe mode
 def systemLoad():  
   pluginsPath = os.listdir( PYFRIEND_PATH_PLUGINS ) 
-  for pluginName in pluginsPath:
-    pluginPath = f"{PYFRIEND_PATH_PLUGINS}//{pluginName}"
+  for pluginPathName in pluginsPath:
+    pluginPath = f"{PYFRIEND_PATH_PLUGINS}//{pluginPathName}"
     if os.path.isdir(pluginPath):
-      if os.path.exists(f"{pluginPath}//{pluginName}.plugin.yml"):
-        pluginConfigFile = open(f"{pluginPath}//{pluginName}.plugin.yml","r")   
+      if os.path.exists(f"{pluginPath}//{pluginPathName}.plugin.yml"):
+        pluginConfigFile = open(f"{pluginPath}//{pluginPathName}.plugin.yml","r")   
         pluginConfig = yaml.load(pluginConfigFile.read(),Loader=yaml.FullLoader)
-        PYFRIEND_CONFIG_PLUGINS[pluginName] = pluginConfig  
+        if os.path.exists(f"{pluginPath}//plugin.py"):
+          if "pluginName" in pluginConfig and pluginConfig.pluginName == pluginPathName
+            pluginName = pluginConfig.pluginName
+            PYFRIEND_CONFIG_PLUGINS[pluginConfig.pluginName] = pluginConfig  
+            infoConsole("INFO","PLUGIN_LOAD",f"PLUGIN[{pluginName}] begins to load .")  
+          else: 
+          infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] .")  
+        else: 
+          infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , the feature implementation section was not found.")  
       else:
-        infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginName}] Unable to load properly.")
+        infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , No configuration information was found.")
 if(__name__=="__main__"):
   try:
     systemLoad()
