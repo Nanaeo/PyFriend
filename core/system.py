9338e007f0229f94fe8667f6286505bd1a2360f6
MliKiowa <nineto0@163.com>
2022-11-05 22:24:51 +0800

    Update system.py

--- core/system.py
+++ core/system.py
@@ -1,5 +1,6 @@
 import helper,sys
-def systemLoad():
+def systemLoad(PYFRIEND_SYSTEM_TOKEN):
   helper.PrintConsole("Welcome","Hello User","The system starts to load and run . ",1)
-  helper.PrintConsole("Check","Vesion","Error",1)
+  PYFRIEND_SYSTEM_VERSION = helper.GlobalDict.getValue(PYFRIEND_SYSTEM_TOKEN,["SYSTEM","VERSION"])
+  helper.PrintConsole("Check","Vesion",PYFRIEND_SYSTEM_VERSION,1)
   return
