ca6c92080d08df1fe49f32e856c50c4a07918081
MliKiowa <nineto0@163.com>
2022-11-05 22:01:09 +0800

    Update GlobalDict.py

--- helper/GlobalDict.py
+++ helper/GlobalDict.py
@@ -28,7 +28,7 @@ def authRegist(name):
 def setValue(authCode , keys, value):
   if not authCode in authTable:
     return False
-  if not keys[0] in authTable[authCode]["range"] and authTable[authCode]["auth"] != 0:
+  if not keys[0] in authTable[authCode]["range"] and authTable[authCode]["auth"] == 1:
     return False
   globalDict = us(globalDict,keys,value)
   return True
