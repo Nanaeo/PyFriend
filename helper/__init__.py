96087a6c1b2f2e279296224fb6a3b97a5e66fc22
MliKiowa <nineto0@163.com>
2022-11-05 17:24:45 +0800

    Update __init__.py

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,3 +1,4 @@
-from PyFriendException import *
-from PluginBase import *
-from LogConsole import *
+# from PyFriendException import *
+# from PluginBase import *
+# from LogConsole import *
+from . import *
