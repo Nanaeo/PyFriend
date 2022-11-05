9dc677f2a79d63c17cfae941791ca270288ccc53
MliKiowa <nineto0@163.com>
2022-11-05 18:21:56 +0800

    Create GlobalVariable.py

---
+++ helper/GlobalVariable.py
@@ -0,0 +1,15 @@
+class GlobalVariable(object):
+  def init():
+    global GLOBALS_DICT
+    GLOBALS_DICT = {}
+  def set(name, value):
+    try:
+        GLOBALS_DICT[name] = value
+        return True
+    except KeyError:
+        return False
+  def get(name):
+    try:
+        return GLOBALS_DICT[name]
+    except KeyError:
+        return " "
