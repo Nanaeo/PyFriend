abae85052b2481920f883d1f028863d8e8f495dd
MliKiowa <nineto0@163.com>
2022-11-05 21:45:43 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def getVar(name):
 try:
   if(__name__=="__main__"):      
     core.systemLoad()
-    print(helper.GlobalDict)
+    print(helper.GlobalDict.authInit)
 except Exception as e:
   errorMsg = traceback.format_exc()
   helper.PrintConsole("FATAL","SYSTEM",f"Specific information :\n {errorMsg}")
