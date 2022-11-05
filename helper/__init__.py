bbbb858ed8703f9522611214894ce21a20ebfa92
MliKiowa <nineto0@163.com>
2022-11-05 21:38:26 +0800

    Update __init__.py

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,3 +1,6 @@
 from .PyFriendException import PyFriendException
 from .PluginBase import PluginBase
 from .LogConsole import PrintConsole
+from .GlobalDict import getValue
+from .GlobalDict import setValue
+from .GlobalDict import auth_init
