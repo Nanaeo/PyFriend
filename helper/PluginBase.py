bea00dd63a4035f9ae3edeebef75f48bf87093e7
MliKiowa <nineto0@163.com>
2022-11-05 23:13:09 +0800

    Update PluginBase.py

--- helper/PluginBase.py
+++ helper/PluginBase.py
@@ -2,22 +2,19 @@ import os, sys
 import yaml
 import importlib
 class PluginBase(object):
-  Plugins = {}
-  def LoadPlugin(PluginPath,pluginFileName):      
+  def LoadPlugin(PYFRIEND_SYSTEM_TOKEN,pluginsPath,pluginPathName):
+    PYFRIEND_PATH_PLUGINS = helper.GlobalDict.getValue(PYFRIEND_SYSTEM_TOKEN,["SYSTEM","PATH_PLUGINS"])
+    pluginPath = f"{PYFRIEND_PATH_PLUGINS}//{pluginPathName}"
     if os.path.isdir(pluginPath):
-      if os.path.exists(f"{pluginPath}//{pluginFileName}.plugin.yml"):
-        pluginConfigFile = open(f"{pluginPath}//{pluginFileName}.plugin.yml","r")   
+      if os.path.exists(f"{pluginPath}//{pluginPathName}.plugin.yml"):
+        pluginConfigFile = open(f"{pluginPath}//{pluginPathName}.plugin.yml","r") 
         pluginConfig = yaml.load(pluginConfigFile.read(),Loader=yaml.FullLoader)
-        pluginConfigFile.close()
         if os.path.exists(f"{pluginPath}//Plugin.py"):        
-          if "pluginName" in pluginConfig:
+          if "pluginName" in pluginConfig :
             pluginName = pluginConfig["pluginName"]
-            Plugins["config"][pluginName] = pluginConfig  
-            Plugins["config"][pluginName]["packageName"] = f"plugins.{pluginName}"
-            infoConsole("INFO","PLUGIN_LOAD",f"PLUGIN[{pluginName}] Basic Info Loaded .",1)  
+            helper.GlobalDict.setValue(PYFRIEND_SYSTEM_TOKEN,[pluginPathName,"CONFIG"],pluginConfig)
+            helper.PrintConsole("INFO","PLUGIN_LOAD",f"PLUGIN[{pluginName}] Basic Info Loaded .",1)  
           else: 
-            infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] .")  
+            helper.PrintConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] .")  
         else: 
-          infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , the feature implementation section was not found.")  
-      else:
-        infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , No configuration information was found.")
+          helper.PrintConsole("ERROR","PLUGIN_LO
