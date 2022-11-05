3ac7647b99b674d2630bf324b33b08cfd1a69acf
MliKiowa <nineto0@163.com>
2022-11-05 20:32:43 +0800

    Update system.py

--- core/system.py
+++ core/system.py
@@ -1,7 +1,5 @@
 import helper,sys
-global PYFRIEND_FRAME
-def systemLoad(PYFRIEND_FRAME_M):
-  PYFRIEND_FRAME = PYFRIEND_FRAME_M
+def systemLoad():
   helper.PrintConsole("Welcome","Hello User","The system starts to load and run . ",1)
-  helper.PrintConsole("Check","Vesion",PYFRIEND_FRAME["Init"]("PYFRIEND_INFO_VERSION"),1)
+  helper.PrintConsole("Check","Vesion","Error",1)
   return
