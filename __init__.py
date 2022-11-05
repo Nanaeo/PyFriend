0ca7be634c535c2102e20255c9950cec1398f97d
MliKiowa <nineto0@163.com>
2022-11-05 15:56:54 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# PyFriend Load Helper
+import * from helper
+import system from .
+# Common variable
+PYFRIEND_INFO_VERSION = "1.0.0"
+PYFRIEND_CONFIG_DEBUG = True
+# Machine generated
+PYFRIEND_PATH_ROOT = os.path.abspath(os.path.dirname(__file__))
+PYFRIEND_PATH_PLUGINS = PYFRIEND_PATH_ROOT + "//plugins"
+# Reference PYFRIEND_PATH_ROOT https://www.cnblogs.com/liangmingshen/p/12794631.html
+if(__name__=="__main__"):
+  try:
+    systemLoad()
+  except Exception as e:
+    errorMsg = traceback.format_exc()   
+    infoConsole("FATAL","SYSTEM",f"Specific information :\n {errorMsg}")
