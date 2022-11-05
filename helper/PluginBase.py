930f41d56c1a5e6f7085a46b2d6e951ba388cd76
MliKiowa <nineto0@163.com>
2022-11-05 15:33:00 +0800

    Create PluginBase.py

---
+++ helper/PluginBase.py
@@ -0,0 +1,21 @@
+import os, sys
+class PluginBase(object):
+  Plugins = {}
+  def LoadPlugin(PluginPath,pluginFileName):      
+    if os.path.isdir(pluginPath):
+      if os.path.exists(f"{pluginPath}//{pluginFileName}.plugin.yml"):
+        pluginConfigFile = open(f"{pluginPath}//{pluginFileName}.plugin.yml","r")   
+        pluginConfig = yaml.load(pluginConfigFile.read(),Loader=yaml.FullLoader)
+        pluginConfigFile.close()
+        if os.path.exists(f"{pluginPath}//Plugin.py"):        
+          if "pluginName" in pluginConfig:
+            pluginName = pluginConfig["pluginName"]
+            Plugins["config"][pluginName] = pluginConfig  
+            Plugins["config"][pluginName]["packageName"] = f"plugins.{pluginName}"
+            infoConsole("INFO","PLUGIN_LOAD",f"PLUGIN[{pluginName}] Basic Info Loaded .",1)  
+          else: 
+            infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] .")  
+        else: 
+          infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , the feature implementation section was not found.")  
+      else:
+        infoConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , No configuration information was found.")
