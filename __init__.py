d6d0544af1027c34a8c4de3c2698ef7fd5ef55d4
MliKiowa <nineto0@163.com>
2022-11-05 19:37:17 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 # PyFriend Load Helper
 import os,sys,traceback
+global PYFRIEND_FRAME
 from helper import PyFriendException
 import core,helper 
 # Common variable
@@ -9,8 +10,11 @@ PYFRIEND_CONFIG_DEBUG = True
 PYFRIEND_PATH_ROOT = os.path.abspath(os.path.dirname(__file__))
 PYFRIEND_PATH_PLUGINS = PYFRIEND_PATH_ROOT + "//plugins"
 # Reference PYFRIEND_PATH_ROOT https://www.cnblogs.com/liangmingshen/p/12794631.html
+def getVar(name):
+  reurn sys._getframe().f_back.f_locals[name]
 try:
   if(__name__=="__main__"):
+    PYFRIEND_FRAME["Init"] = getVar
     core.systemLoad()
 except Exception as e:
   errorMsg = traceback.format_exc()
