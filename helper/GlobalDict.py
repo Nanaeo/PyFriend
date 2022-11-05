ecfa911662b4ea67d45a02fa0e0e2fc921bb5f26
MliKiowa <nineto0@163.com>
2022-11-05 21:14:17 +0800

    Update GlobalDict.py

--- helper/GlobalDict.py
+++ helper/GlobalDict.py
@@ -1,6 +1,6 @@
 import uuid
 # 初始化获取顶级权限
-def _init():
+def auth_init():
   global _global_dict
   global rootCode
   authTable = {}
@@ -8,10 +8,10 @@ def _init():
   rootCode = str(uuid.uuid4())
   authTable[rootCode] = {"name":"system","auth":1,"range":{}} 
   return rootCode
-def regist()
+def auth_regist():
    rootCode = str(uuid.uuid4())
    authTable[rootCode] = {"name":"system","auth":0,"range":{}} 
-def set_value(authCode , key, value):
+def setValue(authCode , key, value):
   if rootCode in authTable:
     tempdata = _global_dict
     if not key[0] in authTable[rootCode]["range"] and authTable[rootCode]["auth"] == 0:
@@ -21,7 +21,7 @@ def set_value(authCode , key, value):
     return True
   else:
     return False
-def get_value(authCode,key):
+def getValue(authCode,key):
   if rootCode in authTable:
   tempdata = _global_dict
   if not key[0] in authTable[rootCode]["range"] and authTable[rootCode]["auth"] == 0:
