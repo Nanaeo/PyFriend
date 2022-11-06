15c58bca8bec176a9251a51432a31e3fb9e17b2c
MliKiowa <nineto0@163.com>
2022-11-06 11:47:30 +0800

    Update PluginBase.py

--- helper/PluginBase.py
+++ helper/PluginBase.py
@@ -3,18 +3,21 @@ import yaml
 import importlib
 class PluginBase(object):
   def LoadPlugin(PYFRIEND_SYSTEM_TOKEN,pluginsPath,pluginPathName):
-    PYFRIEND_PATH_PLUGINS = helper.GlobalDict.getValue(PYFRIEND_SYSTEM_TOKEN,["SYSTEM","PATH_PLUGINS"])
-    pluginPath = f"{PYFRIEND_PATH_PLUGINS}//{pluginPathName}"
-    if os.path.isdir(pluginPath):
-      if os.path.exists(f"{pluginPath}//{pluginPathName}.plugin.yml"):
-        pluginConfigFile = open(f"{pluginPath}//{pluginPathName}.plugin.yml","r") 
-        pluginConfig = yaml.load(pluginConfigFile.read(),Loader=yaml.FullLoader)
-        if os.path.exists(f"{pluginPath}//Plugin.py"):        
-          if "pluginName" in pluginConfig :
-            pluginName = pluginConfig["pluginName"]
-            helper.GlobalDict.setValue(PYFRIEND_SYSTEM_TOKEN,[pluginPathName,"CONFIG"],pluginConfig)
-            helper.PrintConsole("INFO","PLUGIN_LOAD",f"PLUGIN[{pluginName}] Basic Info Loaded .",1)  
-          else: 
-            helper.PrintConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] .")  
+  PYFRIEND_PATH_PLUGINS = helper.GlobalDict.getValue(PYFRIEND_SYSTEM_TOKEN,["SYSTEM","PATH_PLUGINS"])
+  pluginPath = f"{PYFRIEND_PATH_PLUGINS}//{pluginPathName}"
+  if os.path.isdir(pluginPath):
+    if os.path.exists(f"{pluginPath}//{pluginPathName}.plugin.yml"):
+      pluginConfigFile = open(f"{pluginPath}//{pluginPathName}.plugin.yml","r") 
+      pluginConfig = yaml.load(pluginConfigFile.read(),Loader=yaml.FullLoader)
+      if os.path.exists(f"{pluginPath}//Plugin.py"):        
+        if "pluginName" in pluginConfig :
+          pluginName = pluginConfig["pluginName"]
+          helper.GlobalDict.setValue(PYFRIEND_SYSTEM_TOKEN,[pluginPathName,"CONFIG"],pluginConfig)
+          helper.PrintConsole("INFO","PLUGIN_LOAD",f"PLUGIN[{pluginName}] Basic Info Loaded .",1)  
         else: 
-          helper.PrintConsole("ERROR","PLUGIN_LO
+          helper.PrintConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] .")  
+      else: 
+        helper.PrintConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , the feature implementation section was not found.",0)  
+      pluginConfigFile.close()  
+    else:
+      helper.PrintConsole("ERROR","PLUGIN_LOAD",f"PLUGIN[{pluginPathName}] Unable to load properly , No configuration information was found.",0)
