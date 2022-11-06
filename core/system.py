4a63d9dfbe0624e6473e00aef5e641c9042530ef
MliKiowa <nineto0@163.com>
2022-11-06 12:12:33 +0800

    Update system.py

--- core/system.py
+++ core/system.py
@@ -5,7 +5,8 @@ def systemLoad(PYFRIEND_SYSTEM_TOKEN):
   PYFRIEND_SYSTEM_VERSION = helper.GlobalDict.getValue(PYFRIEND_SYSTEM_TOKEN,["SYSTEM","VERSION"])
   helper.PrintConsole("System","Vesion",PYFRIEND_SYSTEM_VERSION,1)
   pluginsPath = os.listdir( PYFRIEND_PATH_PLUGINS ) 
-  PluginBase = helper.PluginBase.PluginBase()
+  PluginBase = helper.PluginBase()
+  print(PluginBase)
   for pluginPathName in pluginsPath:
 # 转入Plugin类加载所有插件 然后公布绑定事件 分析触发事件运行流程     
     PluginBase.LoadPlugin(PYFRIEND_SYSTEM_TOKEN,pluginsPath,pluginPathName)
